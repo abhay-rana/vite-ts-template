99f7f93a45063ed9fb768efbe0464fff873c2195
abhay-rana <abhayrana286@gmail.com>
2023-10-18 00:14:02 +0530

    naming conventions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { useState } from 'react';
+import Footer from '~/components/footer';
 
 function App() {
     return (
         <>
             <div>hello this is vite app</div>
+            <Footer />
         </>
     );
 }
