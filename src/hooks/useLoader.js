8ebe303697af3415062be7b7c9646d460a038688
abhay-rana <abhayrana286@gmail.com>
2025-01-08 12:08:43 +0530

    hooks

--- src/hooks/useLoader.js
+++ src/hooks/useLoader.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+// import React from 'react';
 
 export const useLoader = (initial_state) => {
     const [loader, setLoader] = useState(initial_state);
@@ -8,3 +9,26 @@ export const useLoader = (initial_state) => {
 
     return [loader, startLoader, endLoader];
 };
+
+// Example usage of useLoader hook in a sample component
+
+// const SampleComponent = () => {
+//     const [loader, startLoader, endLoader] = useLoader(false);
+
+//     const handleClick = () => {
+//         startLoader();
+//         // Simulate an async operation
+//         setTimeout(() => {
+//             endLoader();
+//         }, 2000);
+//     };
+
+//     return (
+//         <div>
+//             <button onClick={handleClick}>Load Data</button>
+//             {loader && <p>Loading...</p>}
+//         </div>
+//     );
+// };
+
+// export default SampleComponent;
