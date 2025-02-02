69937c1630e0a397e126e020364fd453fc8af5ba
abhay-rana <abhayrana286@gmail.com>
2025-02-02 15:27:21 +0530

    Add react-hot-toast, dayjs, and antd; update main.tsx for new configurations

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,25 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { Toaster } from 'react-hot-toast';
+
+// Core Library Configurations
+import dayjs from 'dayjs';
+import 'dayjs/locale/en'; // Locale setup for dayjs
+dayjs.locale('en');
+
+// Global Styles
+import 'antd/dist/reset.css'; // Ant Design v5
+import './styles/antd-style.css';
+import './styles/globals.css';
+
 import App from '~/App';
 import store from '~/redux/store';
-import '~/styles/globals.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <>
         <Provider store={store}>
             <App />
+            <Toaster />
         </Provider>
     </>
 );
